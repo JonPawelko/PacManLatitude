c1523d50b5a5bfa69c8061c2492c627f59e63fc9
Jon Pawelko <JonPawelko@yahoo.com>
2024-10-08 08:19:52 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,8 +1,40 @@
 
-function test()
-{
-      var squares = document.querySelectorAll('.square');  // faster to get first?
+var NUM_ROWS = 5;
+var NUM_COLUMNS = 5;
+var current = 0;
+var squares;
 
-      squares[0].innerHTML = "X";
+// key codes
+var SPACE_BAR_KEY = 32;
+var ARROW_LEFT = 37;
+var ARROW_UP = 38;
+var ARROW_RIGHT = 39;
+var ARROW_DOWN = 40;
+
+document.onkeydown = checkKey;
+
+// ---------------------------------------
+
+drawBoard();
+
+// ---------------------------------------
+
+function checkKey(evt)
+{
+  if (evt.keyCode == ARROW_RIGHT)
+  {
+    squares[current++].innerHTML = "";
+    drawBoard();
+  }
+
+}
+
+// -------------------------------------------
+
+function drawBoard()
+{
+      squares = document.querySelectorAll('.square');  // faster to get first?
+
+      squares[current].innerHTML = "X";
 
 }
