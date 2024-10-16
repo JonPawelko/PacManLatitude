08aedad3ee83a9cbe3232d39337f73d2c824e8b6
Jon Pawelko <JonPawelko@yahoo.com>
2024-10-16 06:28:15 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -11,30 +11,122 @@ var ARROW_UP = 38;
 var ARROW_RIGHT = 39;
 var ARROW_DOWN = 40;
 
+const FAIL = 0;
+const SUCCESS = 1;
+
+var walls = new Array;
+
 document.onkeydown = checkKey;
 
 // ---------------------------------------
+// Initialize game components
 
-drawBoard();
+buildWalls();
+
+drawInitialBoard();
 
 // ---------------------------------------
 
 function checkKey(evt)
 {
-  if (evt.keyCode == ARROW_RIGHT)
+  var oldCurr = current;
+
+  if (resolvePacMan(evt.keyCode))
+    redrawBoard(oldCurr, current);
+
+}
+
+// ---------------------------------------
+
+function drawInitialBoard()
+{
+  squares = document.querySelectorAll('.square');  // get squares
+
+  squares[current].innerHTML = "X";   // set PacMan's current position
+
+  for (var i=0; i<NUM_ROWS*NUM_COLUMNS; i++)  // loop through walls array, assume squares empty
   {
-    squares[current++].innerHTML = "";
-    drawBoard();
+    if (walls[i] == 1)
+    {
+      squares[i].innerHTML = "Z";
+    }
   }
 
 }
 
-// -------------------------------------------
+// ---------------------------------------
 
-function drawBoard()
+function buildWalls()
+{
+  for (var i=0; i<NUM_ROWS*NUM_COLUMNS; i++)
+    walls.push(0);
+
+  walls[4] = 1;
+  walls[7] = 1;
+
+}
+
+// --------------------------------------
+
+function redrawBoard(oldSquare, newSquare)
 {
       squares = document.querySelectorAll('.square');  // faster to get first?
 
-      squares[current].innerHTML = "X";
+      squares[oldSquare].innerHTML = "";   // blank for now, but might have 2 ghosts later ***
+
+      squares[newSquare].innerHTML = "X"; // set pac man on new current
 
 }
+
+// ---------------------------------------
+
+function resolvePacMan(direction)
+{
+  switch(direction)
+  {
+
+    case ARROW_RIGHT:
+
+      if ( (((current + 1) % NUM_COLUMNS) != 0) && (walls[current+1] == 0) )
+      {
+        current++;
+        return SUCCESS;
+      }
+      break;
+
+    case ARROW_LEFT:
+
+      if ( ((current % NUM_COLUMNS) != 0) && (walls[current-1] == 0) )
+      {
+        current--;
+        return SUCCESS;
+      }
+      break;
+
+    case ARROW_DOWN:
+
+      if ((current < NUM_COLUMNS * (NUM_ROWS-1)) && (walls[current+NUM_COLUMNS] == 0))
+      {
+        current = current + NUM_COLUMNS;
+        return SUCCESS;
+      }
+      break;
+
+    case ARROW_UP:
+
+      if ((current >= NUM_COLUMNS) && (walls[current-NUM_COLUMNS] == 0))
+      {
+        current = current - NUM_COLUMNS;
+        return SUCCESS;
+      }
+      break;
+
+    default:
+
+      return FAIL;
+
+  } // end switch
+
+} // end function
+
+// -------------------------------------------
